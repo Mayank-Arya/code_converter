f0ef49d6b058f1f6cb5c49abcfb79bbbe08cc99c
Mayank-Arya <ma8183468@gmail.com>
2023-08-05 13:20:31 +0530

    ok

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const {codeRouter} = require('./Routes/converter.routes');
+  
 require('dotenv').config()
 app.use(express.json())
 app.use(cors())
@@ -12,6 +13,26 @@ app.get('/',(req,res) => {
 
 app.use('/code',codeRouter)
 
-app.listen(process.env.PORT || 8000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log('Server Running at',process.env.PORT)
 })
+
+
+// const express = require('express');
+// const app = express();
+// const cors = require('cors');
+// const { codeRouter } = require('./Routes/converter.routes');
+// require('dotenv').config();
+
+// app.use(express.json());
+// app.use(cors()); // Enable CORS for all routes
+
+// app.get('/', (req, res) => {
+//   res.send('Home Page!');
+// });
+
+// app.use('/code', codeRouter);
+
+// app.listen(process.env.PORT, () => {
+//   console.log('Server Running at', process.env.PORT);
+// });
