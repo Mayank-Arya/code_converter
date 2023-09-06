190ad8bc8cb573cb5f894761fccfa50ea1225dd7
Mayank-Arya <ma8183468@gmail.com>
2023-09-06 20:04:34 +0530

    code converter app is ready

---
+++ backend/Routes/converter.routes.js
@@ -0,0 +1,105 @@
+const { Router } = require("express");
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const codeRouter = Router();
+require('dotenv').config();
+
+codeRouter.post('/convert', async (req, res) => {
+  try {
+      let { code, lang } = req.body;
+      let response = await fetch(`https://api.openai.com/v1/chat/completions`, {
+          method: "POST",
+          headers: {
+              "Authorization": `Bearer ${process.env.API_KEY}`,
+              "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+              model: "gpt-3.5-turbo",
+              messages: [{ role: "user", content: `convert this piece of code: ${code} into the code of this ${lang} and only give me the code` }],
+              max_tokens: 1000
+          })
+      });
+
+      response = await response.json();
+
+      // Check if response.choices is defined and not empty
+      if (response.choices && response.choices.length > 0) {
+          const data = response.choices[0].message.content;
+          res.status(200).send({ code: data });
+      } else {
+          // Handle the case when response.choices is empty
+          res.status(500).send({ msg: "No valid response from the API" });
+      }
+  } catch (error) {
+      console.log(error);
+      res.status(500).send({ msg: error.message });
+  }
+})
+
+
+codeRouter.post('/debug', async (req, res) => {
+  try {
+      let { code } = req.body;
+      let response = await fetch(`https://api.openai.com/v1/chat/completions`, {
+          method: "POST",
+          headers: {
+              "Authorization": `Bearer ${process.env.API_KEY}`,
+              "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+              model: "gpt-3.5-turbo",
+              messages: [{ role: "user", content: `debug this piece of code: ${code} and give me the debugged code and the problem with the code` }],
+              max_tokens: 1000
+          })
+      });
+      response = await response.json();
+
+      // Check if response.choices is defined and not empty
+      if (response.choices && response.choices.length > 0) {
+          const data = response.choices[0].message.content;
+          res.status(200).send({ code: data });
+      } else {
+          // Handle the case when response.choices is empty
+          res.status(500).send({ msg: "No valid response from the API" });
+      }
+  } catch (error) {
+      console.log(error);
+      res.status(500).send({ msg: error.message });
+  }
+});
+
+
+codeRouter.post('/quality-checker', async (req, res) => {
+  try {
+      let { code } = req.body;
+      let response = await fetch(`https://api.openai.com/v1/chat/completions`, {
+          method: "POST",
+          headers: {
+              "Authorization": `Bearer ${process.env.API_KEY}`,
+              "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+              model: "gpt-3.5-turbo",
+              messages: [{ role: "user", content: `check the quality of this piece of code: ${code} and generate a short report based on that` }],
+              max_tokens: 1000
+          })
+      });
+      response = await response.json();
+
+      // Check if response.choices is defined and not empty
+      if (response.choices && response.choices.length > 0) {
+          const data = response.choices[0].message.content;
+          res.status(200).send({ report: data });
+      } else {
+          // Handle the case when response.choices is empty
+          res.status(500).send({ msg: "No valid response from the API" });
+      }
+  } catch (error) {
+      console.log(error);
+      res.status(500).send({ msg: error.message });
+  }
+});
+
+
+module.exports = {
+    codeRouter
+}
